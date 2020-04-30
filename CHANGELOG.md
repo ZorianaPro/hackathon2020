3717746823dea56453ee26789681bcf6c191b8a1
Zoriana <zoryana.lesyk@experteer.com>
2020-04-30 15:33:29 +0200

    Add Koa srver

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.0
+* Add koa server
+
 ## 0.1.0
 * Create React App
 
---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+const Koa = require("koa");
+const BodyParser = require("koa-bodyparser");
+const Logger = require("koa-logger");
+const cors = require('koa-cors');
+const Router = require("koa-router");
+const HttpStatus = require("http-status");
+
+const app = new Koa();
+
+const PORT = process.env.PORT || 8082;
+
+app.use(BodyParser());
+app.use(Logger());
+app.use(cors());
+
+const router = new Router();
+
+router.get("/book",async (ctx,next)=>{
+	const books = ["Learn somehting man", "Fluent Python", "Pro Python", "The Go programming language"];
+	ctx.status = HttpStatus.OK;
+	ctx.body = books;
+	await next();
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+
+app.listen(PORT, function () {
+	console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+});
