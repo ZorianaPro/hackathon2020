6a9e1d3a2e41331b2a59d9b2d893b30d4842e636
Zoriana <zoryana.lesyk@experteer.com>
2020-06-04 19:46:55 +0200

    Updates

---
+++ precache-manifest.6ff3da50dc7378b39b91c5287bfec0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5af55d97e7b1cccbf1153fc0fecec3",
+    "url": "/hackathon2020/index.html"
+  },
+  {
+    "revision": "511eb2493a2576f0ee24",
+    "url": "/hackathon2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f3c2ba5fd6621a4c2d2",
+    "url": "/hackathon2020/static/js/2.1741ceee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/hackathon2020/static/js/2.1741ceee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "511eb2493a2576f0ee24",
+    "url": "/hackathon2020/static/js/main.429b3839.chunk.js"
+  },
+  {
+    "revision": "09e68e6f9c1179d075e9",
+    "url": "/hackathon2020/static/js/runtime-main.3736d5e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95ed17f2876b5328b176832ed03d2f1f.js"
+  "/hackathon2020/precache-manifest.6ff3da50dc7378b39b91c5287bfec0e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackathon2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
