1a7055078fc02e2eebc0e7275c9c89bc3c4e9421
yoanad <yoana.dimova@gmail.com>
2020-06-05 23:28:35 +0100

    test lint

--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './rootReducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./rootReducer";
 
 let devAdditions;
 if (module.hot) {
-	module.hot.accept();
+  module.hot.accept();
 }
 
-if ('__REDUX_DEVTOOLS_EXTENSION__' in window) {
-	devAdditions = window.__REDUX_DEVTOOLS_EXTENSION__();
+if ("__REDUX_DEVTOOLS_EXTENSION__" in window) {
+  devAdditions = window.__REDUX_DEVTOOLS_EXTENSION__();
 }
 
-const store = createStore(
-	rootReducer,
-	devAdditions
-);
+const store = createStore(rootReducer, devAdditions);
 
 ReactDOM.render(
-	<React.StrictMode>
-	  <Provider store={ store }>
-		  <App />
-	  </Provider>
-  </React.StrictMode>,
-	document.getElementById('root'));
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,lsasads
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
