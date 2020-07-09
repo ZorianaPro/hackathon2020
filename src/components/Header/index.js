d0c86d887ddfb2fbda9bc5ba856a5085e78e4c9c
yoanad <yoana.dimova@gmail.com>
2020-07-09 18:06:41 +0200

    Wrap in router

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,69 +11,71 @@ const Header = ({}) => {
     `${window.location.pathname}${window.location.hash}` === path;
 
   return (
-    <div className="Header">
-      <ul className="Header-Navigation" onClick={handleClick}>
-        <li>
-          <NavLink
-            to="/#about"
-            smooth={true}
-            isActive={() => {
-              return isNavLinkActive("/#about");
-            }}
-          >
-            About
-          </NavLink>
-        </li>
-        <li>|</li>
-        <li>
-          <NavLink
-            to="/#registration"
-            smooth={true}
-            isActive={() => {
-              return isNavLinkActive("/#registration");
-            }}
-          >
-            Registration
-          </NavLink>
-        </li>
-        <li>|</li>
-        <li>
-          <NavLink
-            to="/#inspiration"
-            smooth={true}
-            isActive={() => {
-              return isNavLinkActive("/#inspiration");
-            }}
-          >
-            Inspiration
-          </NavLink>
-        </li>
-        <li>|</li>
-        <li>
-          <NavLink
-            to="/#ideas"
-            smooth={true}
-            isActive={() => {
-              return isNavLinkActive("/#ideas");
-            }}
-          >
-            Ideas
-          </NavLink>
-        </li>
-        <li>|</li>
-        <li>
-          <NavLink
-            to="/#rules"
-            smooth={true}
-            isActive={() => {
-              return isNavLinkActive("/#rules");
-            }}
-          >
-            Rules
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <Router>
+      <div className="Header">
+        <ul className="Header-Navigation" onClick={handleClick}>
+          <li>
+            <NavLink
+              to="/#about"
+              smooth={true}
+              isActive={() => {
+                return isNavLinkActive("/#about");
+              }}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink
+              to="/#registration"
+              smooth={true}
+              isActive={() => {
+                return isNavLinkActive("/#registration");
+              }}
+            >
+              Registration
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink
+              to="/#inspiration"
+              smooth={true}
+              isActive={() => {
+                return isNavLinkActive("/#inspiration");
+              }}
+            >
+              Inspiration
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink
+              to="/#ideas"
+              smooth={true}
+              isActive={() => {
+                return isNavLinkActive("/#ideas");
+              }}
+            >
+              Ideas
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink
+              to="/#rules"
+              smooth={true}
+              isActive={() => {
+                return isNavLinkActive("/#rules");
+              }}
+            >
+              Rules
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </Router>
   );
 };
 
