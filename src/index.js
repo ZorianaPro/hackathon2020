ab958cdd641456e6897e9bfe808be4c7d02912f5
Zoriana <zoryana.lesyk@experteer.com>
2020-07-21 15:12:06 +0200

    Update homepage

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-
       <Router>
         <App />
       </Router>
