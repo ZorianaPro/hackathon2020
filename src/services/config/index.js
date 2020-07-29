7f63cf071dcbf951e0fe97e3af3b4c84ac71799c
Zoriana <zoryana.lesyk@experteer.com>
2020-07-29 15:41:56 +0200

    Fix errors

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -3,7 +3,7 @@ export const isProduction = process.env.NODE_ENV === 'production';
 export default {
   api: {
     base: isProduction
-      ? ''
+      ? process.env.PUBLIC_URL
       : 'http://localhost:8082',
     ideas: '/ideas',
     members: '/members'
