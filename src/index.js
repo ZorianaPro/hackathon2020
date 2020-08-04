07ac9aa3a7971c0febcad14e2dc655401e24afde
Zoriana <zoryana.lesyk@experteer.com>
2020-08-04 20:46:02 +0200

    change to brovser router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from './store';
 import {
-  HashRouter as Router
+  BrowserRouter as Router
 } from "react-router-dom";
 
 ReactDOM.render(
