d28e76fd6b862b1b67daa7da89ba8a746d09eaa3
Zoriana <zoryana.lesyk@experteer.com>
2020-08-04 21:07:50 +0200

    Add post to API

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -2,7 +2,7 @@ export const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   api: {
-    base: process.env.PUBLIC_URL,
+    base: `${process.env.PUBLIC_URL}:8082`,
     ideas: '/ideas',
     members: '/members'
   }
