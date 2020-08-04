55ebc4c374ce7aae67414651317c9b47d63af35d
Zoriana <zoryana.lesyk@experteer.com>
2020-08-04 21:16:27 +0200

    Remove port

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -2,7 +2,7 @@ export const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   api: {
-    base: `${process.env.PUBLIC_URL}:8082`,
+    base: process.env.PUBLIC_URL,
     ideas: '/ideas',
     members: '/members'
   }
