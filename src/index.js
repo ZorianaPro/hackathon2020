fbcb543625ddc5fa703acf467bebe4b329ec9bdc
Zoriana <zoryana.lesyk@experteer.com>
2020-08-04 20:47:44 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import {
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router  basename={process.env.PUBLIC_URL}>
+      <Router>
         <App />
       </Router>
     </Provider>
