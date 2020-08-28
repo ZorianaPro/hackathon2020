897e971a177f1254a77f060a2c1ae983a25eaa62
Zoriana <zoryanalesyk@gmail.com>
2020-08-28 10:23:48 +0200

    Chane base url

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -1,7 +1,7 @@
 export const isProduction = process.env.NODE_ENV === 'production';
 export default {
   api: {
-    base: isProduction ? process.env.PUBLIC_URL : 'http://localhost:8082',
+    base: process.env.PUBLIC_URL,
     ideas: '/ideas',
     members: '/members'
   }
